85e29780c07e19a79e1a3aaa586948bcc9446bfc
Patrick Veverka <patrick.veverka@gmail.com>
2017-10-25 08:30:11 -0400

    moving the mailers into the web project

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule TicketAgent.Umbrella.Mixfile do
     [
       apps_path: "apps",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -23,5 +24,23 @@ defmodule TicketAgent.Umbrella.Mixfile do
   # and cannot be accessed from applications inside the apps folder
   defp deps do
     []
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      "ecto.setup": [
+        "ecto.create",
+        "ecto.migrate",
+        "run apps/ticket_agent/priv/repo/seeds.exs"
+        ],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
   end
 end
