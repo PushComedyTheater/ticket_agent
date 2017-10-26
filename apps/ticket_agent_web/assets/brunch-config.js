a0aa2511bff0e170f5d3811840e0d017762d7989
Patrick Veverka <patrick.veverka@gmail.com>
2017-10-25 20:54:06 -0400

    brunch config more better

--- apps/ticket_agent_web/assets/brunch-config.js
+++ apps/ticket_agent_web/assets/brunch-config.js
@@ -2,7 +2,17 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: {
+        "js/app.js": [
+          "js/app.js",
+          /^node_modules/,
+        ],
+        "js/admin.js": [
+          "js/admin/*.js",
+          "js/admin/listing/*.js",
+          /^node_modules/,
+        ]
+      }
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
