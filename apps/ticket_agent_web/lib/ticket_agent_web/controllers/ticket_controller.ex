dc9b1bd056ebf203e85a08dc32bf2ec8dda8e469
Patrick Veverka <patrick.veverka@gmail.com>
2017-10-28 12:12:32 -0400

    adding in button

--- apps/ticket_agent_web/lib/ticket_agent_web/controllers/ticket_controller.ex
+++ apps/ticket_agent_web/lib/ticket_agent_web/controllers/ticket_controller.ex
@@ -1,16 +1,20 @@
 defmodule TicketAgentWeb.TicketController do
   use TicketAgentWeb, :controller
 
+  def new(conn, %{"show_id" => id, "guest_checkout" => "true"}), do: render_new(conn, id)
   def new(conn, %{"show_id" => id}) do
     if !Coherence.logged_in?(conn) do
       redirect(
         conn,
         to: registration_path(
-          conn, :new, [redirect_url: ticket_path(conn, :new, [show_id: id])]))
-      # redirect conn, to: session_path() "/session/new?redirect=/tickets/new?show_id=#{id}"
-    else
-      IO.inspect "LOGGED IN"
+          conn, :new, [redirect_url: ticket_path(conn, :new, [show_id: id])]
+        )
+      )
     end
+    render_new(conn, id)
+  end
+
+  defp render_new(conn, show_id) do
     render conn, "new.html"
   end
 end
