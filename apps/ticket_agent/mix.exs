1f5131c7aeba5230b450c5a9aef33d8346d0c30b
Patrick Veverka <patrick.veverka@gmail.com>
2018-02-15 09:38:34 -0500

    bump version

--- apps/ticket_agent/mix.exs
+++ apps/ticket_agent/mix.exs
@@ -4,7 +4,7 @@ defmodule TicketAgent.Mixfile do
   def project do
     [
       app: :ticket_agent,
-      version: append_revision("0.0.5"),
+      version: append_revision("0.0.6"),
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
