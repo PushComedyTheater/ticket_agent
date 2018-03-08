f6364e24b98f56bba6108e2373ee67f365bda8fc
Patrick Veverka <patrick.veverka@gmail.com>
2018-03-08 10:22:34 -0500

    adding in files

--- apps/ticket_agent/mix.exs
+++ apps/ticket_agent/mix.exs
@@ -4,7 +4,7 @@ defmodule TicketAgent.Mixfile do
   def project do
     [
       app: :ticket_agent,
-      version: append_revision("0.0.8"),
+      version: append_revision("0.0.9"),
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
