a1647b46b63727cbcce96b7fbe946da5d7cfe4ef
Patrick Veverka <patrick.veverka@gmail.com>
2018-04-10 14:59:15 -0400

    lots of changes

--- apps/ticket_agent/config/config.exs
+++ apps/ticket_agent/config/config.exs
@@ -15,7 +15,7 @@ sha = String.trim(sha)
 config :ticket_agent, :release, sha
 
 config :sentry,
-  dsn: System.get_env("SENTRY_PRIVATE_DSN"),
+  dsn: System.get_env("SENTRY_PRIVATE_DSN") || "${SENTRY_PRIVATE_DSN}",
   environment_name: Mix.env,
   enable_source_code_context: true,
   root_source_code_path: File.cwd!,
@@ -90,46 +90,46 @@ config :cloudinex,
   debug: false,
   base_url: "https://api.cloudinary.com/v1_1/",
   base_image_url: "https://res.cloudinary.com/",
-  api_key: System.get_env("CLOUDINEX_API_KEY"),
-  secret: System.get_env("CLOUDINEX_SECRET"),
+  api_key: System.get_env("CLOUDINEX_API_KEY") || "${CLOUDINEX_API_KEY}",
+  secret: System.get_env("CLOUDINEX_SECRET") || "${CLOUDINEX_SECRET}",
   cloud_name: "push-comedy-theater" #no default
 
 config :oauth2, debug: true
 
 config :ticket_agent, Amazon,
-  client_id: System.get_env("AMAZON_CLIENT_ID"),
-  client_secret: System.get_env("AMAZON_CLIENT_SECRET"),
-  redirect_uri: System.get_env("AMAZON_REDIRECT_URI")
+  client_id: System.get_env("AMAZON_CLIENT_ID") || "${AMAZON_CLIENT_ID}",
+  client_secret: System.get_env("AMAZON_CLIENT_SECRET") || "${AMAZON_CLIENT_SECRET}",
+  redirect_uri: System.get_env("AMAZON_REDIRECT_URI") || "${AMAZON_REDIRECT_URI}"
 
 config :ticket_agent, Facebook,
-  client_id: System.get_env("FACEBOOK_CLIENT_ID"),
-  client_secret: System.get_env("FACEBOOK_CLIENT_SECRET"),
-  redirect_uri: System.get_env("FACEBOOK_REDIRECT_URI")
+  client_id: System.get_env("FACEBOOK_CLIENT_ID") || "${FACEBOOK_CLIENT_ID}",
+  client_secret: System.get_env("FACEBOOK_CLIENT_SECRET") || "${FACEBOOK_CLIENT_SECRET}",
+  redirect_uri: System.get_env("FACEBOOK_REDIRECT_URI") || "${FACEBOOK_REDIRECT_URI}"
 
 config :ticket_agent, Google,
-  client_id: System.get_env("GOOGLE_CLIENT_ID"),
-  client_secret: System.get_env("GOOGLE_CLIENT_SECRET"),
-  redirect_uri: System.get_env("GOOGLE_REDIRECT_URI")
+  client_id: System.get_env("GOOGLE_CLIENT_ID") || "${GOOGLE_CLIENT_ID}",
+  client_secret: System.get_env("GOOGLE_CLIENT_SECRET") || "${GOOGLE_CLIENT_SECRET}",
+  redirect_uri: System.get_env("GOOGLE_REDIRECT_URI") || "${GOOGLE_REDIRECT_URI}"
 
 config :ticket_agent, LinkedIn,
-  client_id: System.get_env("LINKEDIN_CLIENT_ID"),
-  client_secret: System.get_env("LINKEDIN_CLIENT_SECRET"),
-  redirect_uri: System.get_env("LINKEDIN_REDIRECT_URI")
+  client_id: System.get_env("LINKEDIN_CLIENT_ID") || "${LINKEDIN_CLIENT_ID}",
+  client_secret: System.get_env("LINKEDIN_CLIENT_SECRET") || "${LINKEDIN_CLIENT_SECRET}",
+  redirect_uri: System.get_env("LINKEDIN_REDIRECT_URI") || "${LINKEDIN_REDIRECT_URI}"
 
 config :ticket_agent, Microsoft,
-  client_id: System.get_env("MICROSOFT_CLIENT_ID"),
-  client_secret: System.get_env("MICROSOFT_CLIENT_SECRET"),
-  redirect_uri: System.get_env("MICROSOFT_REDIRECT_URI")
+  client_id: System.get_env("MICROSOFT_CLIENT_ID") || "${MICROSOFT_CLIENT_ID}",
+  client_secret: System.get_env("MICROSOFT_CLIENT_SECRET") || "${MICROSOFT_CLIENT_SECRET}",
+  redirect_uri: System.get_env("MICROSOFT_REDIRECT_URI") || "${MICROSOFT_REDIRECT_URI}"
 
 config :ticket_agent, Twitter,
-  consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
-  consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
-  redirect_uri: System.get_env("TWITTER_REDIRECT_URI")
+  consumer_key: System.get_env("TWITTER_CONSUMER_KEY") || "${TWITTER_CONSUMER_KEY}",
+  consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET") || "${TWITTER_CONSUMER_SECRET}",
+  redirect_uri: System.get_env("TWITTER_REDIRECT_URI") || "${TWITTER_REDIRECT_URI}"
 
 config :ticket_agent, Universe,
-  client_id: System.get_env("UNIVERSE_CLIENT_ID"),
-  client_secret: System.get_env("UNIVERSE_CLIENT_SECRET"),
-  redirect_uri: System.get_env("UNIVERSE_REDIRECT_URI")  
+  client_id: System.get_env("UNIVERSE_CLIENT_ID") || "${UNIVERSE_CLIENT_ID}",
+  client_secret: System.get_env("UNIVERSE_CLIENT_SECRET") || "${UNIVERSE_CLIENT_SECRET}",
+  redirect_uri: System.get_env("UNIVERSE_REDIRECT_URI") || "${UNIVERSE_REDIRECT_URI}"
 
 value = case System.get_env("TICKET_LOCK_LENGTH") do
   nil -> 302
@@ -138,7 +138,7 @@ end
 config :ticket_agent, :ticket_lock_length, value
 
 config :ticket_agent, Stripe,
-  secret_key: System.get_env("STRIPE_SECRET_KEY"),
-  publishable_key: System.get_env("STRIPE_PUBLISHABLE_KEY")
+  secret_key: System.get_env("STRIPE_SECRET_KEY") || "${STRIPE_SECRET_KEY}",
+  publishable_key: System.get_env("STRIPE_PUBLISHABLE_KEY") || "${STRIPE_PUBLISHABLE_KEY}"
 
-config :ticket_agent, :stripe_api_url, System.get_env("STRIPE_API_URL")
+config :ticket_agent, :stripe_api_url, System.get_env("STRIPE_API_URL") || "${STRIPE_API_URL}"
