790bdb59a563cd587a5b024cc8dd5624d5ad200c
Patrick Veverka <patrick.veverka@gmail.com>
2018-04-29 15:10:52 -0400

    0.1.1

--- apps/ticket_agent_web/mix.exs
+++ apps/ticket_agent_web/mix.exs
@@ -4,7 +4,7 @@ defmodule TicketAgentWeb.Mixfile do
   def project do
     [
       app: :ticket_agent_web,
-      version: append_revision("0.1.0"),
+      version: append_revision("0.1.1"),
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
