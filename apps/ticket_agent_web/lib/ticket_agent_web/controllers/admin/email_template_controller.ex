37283d7b6e190da84540343c1b6b1b7eb272e74f
Patrick Veverka <patrick.veverka@gmail.com>
2018-08-06 15:44:18 -0400

    email

--- apps/ticket_agent_web/lib/ticket_agent_web/controllers/admin/email_template_controller.ex
+++ apps/ticket_agent_web/lib/ticket_agent_web/controllers/admin/email_template_controller.ex
@@ -1,12 +1,35 @@
 defmodule TicketAgentWeb.Admin.EmailTemplateController do
   use TicketAgentWeb, :controller
   require Logger
+  import Ecto.Query
   alias TicketAgent.Generators.OrderPdfGenerator
-  alias TicketAgent.{Order, Repo, Ticket}
-  alias TicketAgent.Emails.OrderEmail
+  alias TicketAgent.{Listing, Order, Repo, Ticket}
+  alias TicketAgent.Emails.{OneTimeLogin, OrderEmail, UserWelcomeEmail}
 
   @host         Application.get_env(:ticket_agent, :email_base_url, "https://pushcomedytheater.com")
   @template_dir Application.app_dir(:ticket_agent, "priv/email_templates")
+
+  def show(conn, %{"type" => "user_welcome_email"}) do
+    current_user = Coherence.current_user(conn)
+    email = UserWelcomeEmail.welcome_email(current_user.name, current_user.email)
+
+    conn
+    |> put_resp_content_type("text/html")
+    |> send_resp(200, email.html_body)
+
+  end
+
+  def show(conn, %{"type" => "one_time_login"}) do
+    current_user = Coherence.current_user(conn)
+
+    listing = Repo.one(from x in Listing, order_by: [desc: x.id], limit: 1)
+
+    email = OneTimeLogin.one_time_login(current_user, listing.id)
+
+    conn
+    |> put_resp_content_type("text/html")
+    |> send_resp(200, email.html_body)
+  end
 
   def show(conn, %{"type" => "order_pdf", "id" => order_slug}) do
     value =
